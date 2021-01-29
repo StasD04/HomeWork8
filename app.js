bb8a8ef8f8e6457186e569525f96ddcbd9b392ca
StasD04 <76402277+StasD04@users.noreply.github.com>
2021-01-29 16:59:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+function colorgenerate() {
+    return Math.floor(Math.random() * 256)
+}
 function colorchanger () {
     var color = document.getElementById("lebedev")
-    var red = Math.floor(Math.random() * 256)
-    var green = Math.floor(Math.random() * 256)
-    var blue = Math.floor(Math.random() * 256)
+    var red = colorgenerate()
+    var green = colorgenerate()
+    var blue = colorgenerate()
     color.style.backgroundColor=`rgb(${red}, ${green}, ${blue})`;
     if ( (red * 0.3) + (green * 0.59) + (blue * 0.11) <70 ){
         color.style.color="white";}
