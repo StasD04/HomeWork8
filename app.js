41b6c7796cfec87143ed33cc5c5c83c4527df2f7
StasD04 <76402277+StasD04@users.noreply.github.com>
2021-01-30 03:12:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-function colorgenerate() {
+function colorGenerate() {
     return Math.floor(Math.random() * 256)
 }
-function colorchanger () {
-    var color = document.getElementById("lebedev")
-    var red = colorgenerate()
-    var green = colorgenerate()
-    var blue = colorgenerate()
-    color.style.backgroundColor=`rgb(${red}, ${green}, ${blue})`;
+let leb = document.getElementById('lebedev')
+function colorChanger () {
+    let red = colorGenerate()
+    let green = colorGenerate()
+    let blue = colorGenerate()
+    leb.style.backgroundColor=`rgb(${red}, ${green}, ${blue})`
     if ( (red * 0.3) + (green * 0.59) + (blue * 0.11) <70 ){
-        color.style.color="white";}
-    else {color.style.color="black";}
+        leb.style.color="white"}
+    else {leb.style.color="black"}
 }
-setTimeout(colorchanger,10)
+colorChanger()
+leb.addEventListener("mouseover", function () {  colorChanger() })
